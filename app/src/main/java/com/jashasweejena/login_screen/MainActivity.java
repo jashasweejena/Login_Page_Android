cdd462760e927610a220af82b53dd01f5b4cb86b
Jashaswee Jena <j.ishujena2000@gmail.com>
2019-01-21 09:27:01 +0530

    Anims working properly in wcomeTxtView

--- app/src/main/java/com/jashasweejena/login_screen/MainActivity.java
+++ app/src/main/java/com/jashasweejena/login_screen/MainActivity.java
@@ -2,9 +2,12 @@ package com.jashasweejena.login_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.hanks.htextview.base.AnimationListener;
 import com.hanks.htextview.base.HTextView;
 
 import java.util.ArrayList;
@@ -15,28 +18,46 @@ public class MainActivity extends AppCompatActivity {
     HTextView wcomeTxtView;
     HTextView signTxtView;
     String[] listOfWords;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listOfWords = new String[]{"Namaskar,", "Namaste,", "Hola,", "Nǐ hǎo,", "Bonjour,"};
+        listOfWords = new String[]{"Namaskar,", "Namaste,", "Hola,", "Bonjour,", "Nǐ hǎo,"};
         getSupportActionBar().hide();
         View decorView = getWindow().getDecorView();
 // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
 
-        wcomeTxtView = (HTextView) findViewById(R.id.textView2);
-        signTxtView = (HTextView) findViewById(R.id.textView);
-        try {
-            Thread.sleep(200);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        for(String x : listOfWords){
-            wcomeTxtView.animateText(x);
-        }
+        wcomeTxtView = (com.hanks.htextview.evaporate.EvaporateTextView) findViewById(R.id.textView2);
+        signTxtView = (com.hanks.htextview.evaporate.EvaporateTextView) findViewById(R.id.textView);
+        wcomeTxtView.setAnimationListener(new AnimationListener() {
+            @Override
+            public void onAnimationEnd(HTextView hTextView) {
+
+            }
+        });
+        signTxtView.setAnimationListener(new AnimationListener() {
+            @Override
+            public void onAnimationEnd(HTextView hTextView) {
+
+            }
+        });
+
+       final Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                wcomeTxtView.animateText(listOfWords[i++]);
+
+                if(i < listOfWords.length - 1) {
+                    h.postDelayed(this, 1000);
+                    Log.d("Runnable", "run: " + listOfWords[i]);
+                }
+            }
+        }, 200);
         mLogo = findViewById(R.id.logo_imgview);
         mLogo.setImageResource(R.drawable.logo_dark);
 
