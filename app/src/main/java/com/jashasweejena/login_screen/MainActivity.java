df5e163c8783dd6dc768d351c6ecd86e347ecb26
Jashaswee Jena <j.ishujena2000@gmail.com>
2019-01-21 08:15:27 +0530

    new

--- app/src/main/java/com/jashasweejena/login_screen/MainActivity.java
+++ app/src/main/java/com/jashasweejena/login_screen/MainActivity.java
@@ -2,46 +2,49 @@ package com.jashasweejena.login_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.hanks.htextview.base.AnimationListener;
 import com.hanks.htextview.base.HTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ImageView mLogo;
+    HTextView wcomeTxtView;
+    HTextView signTxtView;
+    String[] listOfWords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        listOfWords = new String[]{"Namaskar,", "Namaste,", "Hola,", "Nǐ hǎo,", "Bonjour,"};
         getSupportActionBar().hide();
         View decorView = getWindow().getDecorView();
 // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
 
-        final HTextView textView = (HTextView) findViewById(R.id.textView2);
-        final Handler h = new Handler();
-        h.post(new Runnable() {
-            int i = 0;
-            @Override
-            public void run() {
-                textView.animateText("Welcome back");
-                if(i == 1){ // just remove call backs
-                    h.removeCallbacks(this);
-                    Log.d("Runnable","ok");
-                } else { // post again
-                    i++;
-                    h.postDelayed(this, 1000);
-                }
-            }
-        });
+        wcomeTxtView = (HTextView) findViewById(R.id.textView2);
+        signTxtView = (HTextView) findViewById(R.id.textView);
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for(String x : listOfWords){
+            wcomeTxtView.animateText(x);
+        }
         mLogo = findViewById(R.id.logo_imgview);
         mLogo.setImageResource(R.drawable.logo_dark);
 
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
