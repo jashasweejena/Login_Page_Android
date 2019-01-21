0877c1372c46e01966abed852e587221cb0ecc1b
Jashaswee Jena <j.ishujena2000@gmail.com>
2019-01-21 16:42:26 +0530

    Add Dark Mode

--- app/src/main/java/com/jashasweejena/login_screen/MainActivity.java
+++ app/src/main/java/com/jashasweejena/login_screen/MainActivity.java
@@ -1,11 +1,16 @@
 package com.jashasweejena.login_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 
 import com.hanks.htextview.base.AnimationListener;
 import com.hanks.htextview.base.HTextView;
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView mLogo;
     HTextView wcomeTxtView;
     HTextView signTxtView;
+    Switch mSwitch;
     String[] listOfWords;
     int i = 0;
     boolean isComplete = false;
@@ -24,29 +30,50 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.darktheme);
+        }
+        else {
+            setTheme(R.style.AppTheme);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLogo = findViewById(R.id.logo_imgview);
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            mLogo.setImageResource(R.drawable.logo_dark);
+        }
+        else{
+            mLogo.setImageResource(R.drawable.logo_light);
+
+        }
+        mSwitch = findViewById(R.id.mySwitch);
+
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            mSwitch.setChecked(true);
+        }
+
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                   darkMode();
+                }
+                else{
+                    lightMode();
+                }
+            }
+        });
         listOfWords = new String[]{"ନମସ୍କାର,", "নমস্কার", "नमस्ते,", "வணக்கம்,", "నమస్కారం,", "Hola,", "Bonjour,", "Hello !"};
         getSupportActionBar().hide();
         View decorView = getWindow().getDecorView();
-// Hide the status bar.
+        // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
 
         wcomeTxtView = (com.hanks.htextview.evaporate.EvaporateTextView) findViewById(R.id.textView2);
         signTxtView = (com.hanks.htextview.evaporate.EvaporateTextView) findViewById(R.id.textView);
-        wcomeTxtView.setAnimationListener(new AnimationListener() {
-            @Override
-            public void onAnimationEnd(HTextView hTextView) {
-
-            }
-        });
-        signTxtView.setAnimationListener(new AnimationListener() {
-            @Override
-            public void onAnimationEnd(HTextView hTextView) {
-
-            }
-        });
 
        final Handler h = new Handler();
         h.postDelayed(new Runnable() {
@@ -65,11 +92,27 @@ public class MainActivity extends AppCompatActivity {
         }, 200);
 
         mLogo = findViewById(R.id.logo_imgview);
-        mLogo.setImageResource(R.drawable.logo_dark);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    public void restartApp(){
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void darkMode(){
+        mLogo.setImageResource(R.drawable.logo_light);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        restartApp();
+    }
+    public void lightMode(){
+        mLogo.setImageResource(R.drawable.logo_dark);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        restartApp();
     }
 }
